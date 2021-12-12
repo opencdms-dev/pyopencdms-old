3c2689b9f1421a126ff8fd661349f77349372f06
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-12 16:25:22 +0600

    run all tests using pytest

--- tests/integration/test_midas.py
+++ tests/integration/test_midas.py
@@ -7,25 +7,30 @@ from sqlalchemy.sql import text as sa_text
 from sqlalchemy.orm import sessionmaker
 from opencdms.models.midas import core as midas_models
 from test_util import get_midas_connection_string
+from faker import Faker
 
 DB_URL = get_midas_connection_string()
-
+fake = Faker()
 
 db_engine = create_engine(DB_URL)
 
-equipment_data = dict(
-    equipment_id=random.randint(1, 10000),
-    equipment_type_id=random.randint(1, 10000),
-    manufacturer_name=uuid.uuid4().hex[:28],
-    manufacturer_sn_txt=uuid.uuid4().hex[:28],
-    met_ref_txt=uuid.uuid4().hex[:28],
-    eqpt_prct_date=datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S"),
-    equipment_cost=random.random()*10000,
-    eqpt_dspl_date=datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S"),
-    eqpt_dspl_rmrk=uuid.uuid4().hex,
-    eqpt_last_updated_date=datetime.datetime.utcnow().strftime(
-        "%Y:%m:%d %H:%M:%S"
-    )
+source_data = dict(
+    src_id=random.randint(1, 10000),
+    src_name=uuid.uuid4().hex,
+    high_prcn_lat=fake.latitude(),
+    high_prcn_lon=fake.longitude(),
+    loc_geog_area_id=uuid.uuid4().hex[:4],
+    rec_st_ind=random.randint(1, 1000),
+    src_bgn_date=datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S"),
+    src_type=uuid.uuid4().hex[:15],
+    grid_ref_type=random.choice(['CI', 'IRL', 'OS', 'XX', 'ROI']),
+    east_grid_ref=random.randint(1, 100000),
+    north_grid_ref=random.randint(1, 100000),
+    hydr_area_id = random.randint(1, 1000),
+    post_code = uuid.uuid4().hex[:9],
+    src_end_date = datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S"),
+    elevation = random.random()*100,
+    wmo_region_code = random.choice(["A", "B", "S", "Q", "W", "E", "R", "T"]),
 )
 
 
@@ -47,7 +52,7 @@ def setup_module(module):
             db_engine.execute(
                 sa_text(
                     f'''
-                    TRUNCATE TABLE {midas_models.Equipment.__tablename__}
+                    TRUNCATE TABLE {midas_models.Source.__tablename__}
                     RESTART IDENTITY CASCADE
                     '''
                 ).execution_options(autocommit=True)
@@ -56,7 +61,7 @@ def setup_module(module):
     Session = sessionmaker(bind=db_engine)
     session = Session()
 
-    session.add(midas_models.Equipment(**equipment_data))
+    session.add(midas_models.Source(**source_data))
 
     session.commit()
     session.close()
@@ -71,57 +76,57 @@ def teardown_module(module):
         with connection.begin():
             db_engine.execute(
                 sa_text(
-                    f'''TRUNCATE TABLE {midas_models.Equipment.__tablename__}
+                    f'''TRUNCATE TABLE {midas_models.Source.__tablename__}
                      RESTART IDENTITY CASCADE'''
                 ).execution_options(autocommit=True)
             )
 
 
 @pytest.mark.order(100)
-def test_should_create_a_equipment(db_session):
-    equipment = midas_models.Equipment(**equipment_data)
-    db_session.add(equipment)
+def test_should_create_a_source(db_session):
+    source = midas_models.Source(**source_data)
+    db_session.add(source)
     db_session.commit()
 
-    assert equipment.equipment_id == equipment_data['equipment_id']
+    assert source.source_id == source_data['source_id']
 
 
 @pytest.mark.order(101)
-def test_should_read_all_equipments(db_session):
-    equipments = db_session.query(midas_models.Equipment).all()
+def test_should_read_all_sources(db_session):
+    sources = db_session.query(midas_models.Source).all()
 
-    for equipment in equipments:
-        assert isinstance(equipment, midas_models.Equipment)
+    for source in sources:
+        assert isinstance(source, midas_models.Source)
 
 
 @pytest.mark.order(102)
-def test_should_return_a_single_equipment(db_session):
-    equipment = db_session.query(midas_models.Equipment)\
-        .get(equipment_data['equipment_id'])
+def test_should_return_a_single_source(db_session):
+    source = db_session.query(midas_models.Source)\
+        .get(source_data['source_id'])
 
-    assert equipment.equipment_id == equipment_data['equipment_id']
+    assert source.source_id == source_data['source_id']
 
 
 @pytest.mark.order(103)
-def test_should_update_equipment(db_session):
-    db_session.query(midas_models.Equipment)\
-        .filter_by(equipment_id=equipment_data['equipment_id'])\
+def test_should_update_source(db_session):
+    db_session.query(midas_models.Source)\
+        .filter_by(source_id=source_data['source_id'])\
         .update({'region': 'US'})
     db_session.commit()
 
-    updated_equipment = db_session.query(midas_models.Equipment)\
-        .get(equipment_data['equipment_id'])
+    updated_source = db_session.query(midas_models.Source)\
+        .get(source_data['source_id'])
 
-    assert updated_equipment.region == 'US'
+    assert updated_source.region == 'US'
 
 
 @pytest.mark.order(104)
-def test_should_delete_equipment(db_session):
-    db_session.query(midas_models.Equipment)\
-        .filter_by(equipment_id=equipment_data['equipment_id']).delete()
+def test_should_delete_source(db_session):
+    db_session.query(midas_models.Source)\
+        .filter_by(source_id=source_data['source_id']).delete()
     db_session.commit()
 
-    deleted_equipment = db_session.query(midas_models.Equipment)\
-        .get(equipment_data['equipment_id'])
+    deleted_source = db_session.query(midas_models.Source)\
+        .get(source_data['source_id'])
 
-    assert deleted_equipment is None
+    assert deleted_source is None
