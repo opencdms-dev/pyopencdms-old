d6dff6844a153693205e85b041e338f6037a395b
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-16 13:41:07 +0600

    write a common provider

--- opencdms/provider/opencdms.py
+++ opencdms/provider/opencdms.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Union, Any
 from sqlalchemy.orm.session import Session
 from opencdms.provider.clide import ClideProvider
 from opencdms.provider.climsoft import Climsoft4Provider
@@ -41,16 +41,215 @@ class OpenCDMSProvider:
         model_name: str,
         data: Dict
     ):
+        response = {
+            "clide": None,
+            "climsoft": None,
+            "mch": None,
+            "midas": None
+        }
         if self.clide_provider is not None:
             with clide_session() as db_session:
-                self.clide_provider.create(db_session, model_name, data)
+                response["clide"] = self.clide_provider.create(
+                    db_session, model_name, data
+                )
         if self.climsoft_provider is not None:
             with climsoft_session() as db_session:
-                self.climsoft_provider.create(db_session, model_name, data)
+                response["climsoft"] = self.climsoft_provider.create(
+                    db_session,
+                    model_name,
+                    data
+                )
         if self.mch_provider is not None:
             with mch_session() as db_session:
-                self.mch_provider.create(db_session, model_name, data)
+                response["mch"] = self.mch_provider.create(
+                    db_session,
+                    model_name,
+                    data
+                )
         if self.midas_provider is not None:
             with midas_session() as db_session:
-                self.midas_provider.create(db_session, model_name, data)
+                response["midas"] = self.midas_provider.create(
+                    db_session,
+                    model_name,
+                    data
+                )
 
+        return response
+
+    def get(self, model_name: str, unique_id: Dict[str, Union[str, int]]):
+        response = {
+            "clide": None,
+            "climsoft": None,
+            "mch": None,
+            "midas": None
+        }
+
+        if self.clide_provider is not None:
+            with clide_session() as db_session:
+                response["clide"] = self.clide_provider.get(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.climsoft_provider is not None:
+            with climsoft_session() as db_session:
+                response["climsoft"] = self.climsoft_provider.get(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.mch_provider is not None:
+            with mch_session() as db_session:
+                response["mch"] = self.mch_provider.get(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.midas_provider is not None:
+            with midas_session() as db_session:
+                response["midas"] = self.midas_provider.get(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+
+        return response
+
+    def list(
+        self,
+        model_name: str,
+        query: Dict[str, Dict[str, Any]] = None,
+        limit: int = 25,
+        offset: int = 0
+    ):
+        response = {
+            "clide": None,
+            "climsoft": None,
+            "mch": None,
+            "midas": None
+        }
+
+        if self.clide_provider is not None:
+            with clide_session() as db_session:
+                response["clide"] = self.clide_provider.list(
+                    db_session,
+                    model_name,
+                    query,
+                    limit,
+                    offset
+                )
+        if self.climsoft_provider is not None:
+            with climsoft_session() as db_session:
+                response["climsoft"] = self.climsoft_provider.list(
+                    db_session,
+                    model_name,
+                    query,
+                    limit,
+                    offset
+                )
+        if self.mch_provider is not None:
+            with mch_session() as db_session:
+                response["mch"] = self.mch_provider.list(
+                    db_session,
+                    model_name,
+                    query,
+                    limit,
+                    offset
+                )
+        if self.midas_provider is not None:
+            with midas_session() as db_session:
+                response["midas"] = self.midas_provider.list(
+                    db_session,
+                    model_name,
+                    query,
+                    limit,
+                    offset
+                )
+        return response
+
+    def update(
+        self,
+        model_name: str,
+        unique_id: Dict[str, Union[str, int]],
+        data: dict
+    ):
+        response = {
+            "clide": None,
+            "climsoft": None,
+            "mch": None,
+            "midas": None
+        }
+
+        if self.clide_provider is not None:
+            with clide_session() as db_session:
+                response["clide"] = self.clide_provider.update(
+                    db_session,
+                    model_name,
+                    unique_id,
+                    data
+                )
+        if self.climsoft_provider is not None:
+            with climsoft_session() as db_session:
+                response["climsoft"] = self.climsoft_provider.update(
+                    db_session,
+                    model_name,
+                    unique_id,
+                    data
+                )
+        if self.mch_provider is not None:
+            with mch_session() as db_session:
+                response["mch"] = self.mch_provider.update(
+                    db_session,
+                    model_name,
+                    unique_id,
+                    data
+                )
+        if self.midas_provider is not None:
+            with midas_session() as db_session:
+                response["midas"] = self.midas_provider.update(
+                    db_session,
+                    model_name,
+                    unique_id,
+                    data
+                )
+
+        return response
+
+    def delete(self, model_name: str, unique_id: Dict[str, Union[str, int]]):
+        response = {
+            "clide": None,
+            "climsoft": None,
+            "mch": None,
+            "midas": None
+        }
+
+        if self.clide_provider is not None:
+            with clide_session() as db_session:
+                response["clide"] = self.clide_provider.delete(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.climsoft_provider is not None:
+            with climsoft_session() as db_session:
+                response["climsoft"] = self.climsoft_provider.delete(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.mch_provider is not None:
+            with mch_session() as db_session:
+                response["mch"] = self.mch_provider.delete(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+        if self.midas_provider is not None:
+            with midas_session() as db_session:
+                response["midas"] = self.midas_provider.delete(
+                    db_session,
+                    model_name,
+                    unique_id
+                )
+
+        return response
