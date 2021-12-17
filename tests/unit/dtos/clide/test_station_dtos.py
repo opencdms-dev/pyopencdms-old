3a5335ba0384b15985d73b624bdbd881b83e1687
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-18 01:59:10 +0600

    fix clide station dto test

--- tests/unit/dtos/clide/test_station_dtos.py
+++ tests/unit/dtos/clide/test_station_dtos.py
@@ -1,4 +1,4 @@
-from opencdms.dtos.clide import station_schema as clide_station
+from opencdms.dtos.clide import station as clide_station
 from opencdms.dtos.opencdms import station_schema as opencdms_station
 from tests.unit.dtos import data
 
@@ -8,7 +8,3 @@ def test_can_convert_common_station_to_clide_station():
     clide_stn = clide_station.Station(**data.station_data)
 
     assert opencdms_stn.dict() != clide_stn.dict()
-
-#
-# def test_can_convert_clide_station_to_common_station():
-#     assert False
