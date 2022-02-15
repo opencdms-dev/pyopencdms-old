f49cace65a53f3e46d0d3bc030cc8965f84183fb
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-02-15 13:36:37 +0600

    fix sync with sqlalchemy

--- opencdms/utils/climsoft/staionelement.py
+++ opencdms/utils/climsoft/staionelement.py
@@ -1,34 +1,76 @@
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql.functions import func
-from sqlalchemy.sql import select, insert
 from opencdms.models.climsoft.v4_1_1_core import (
     Observationfinal,
     Stationelement
 )
 
 
-def sync_stationelement_with_observationfinal_sqla(db_session: Session):
-    db_session.execute(f"TRUNCATE {Stationelement.__tablename__}")
-    db_session.commit()
-
-    select_stmt = select(
+def group_observation_final_data(db_session: Session):
+    subquery = db_session.query(
         Observationfinal.recordedFrom,
         Observationfinal.describedBy,
         func.DATE(Observationfinal.obsDatetime).label("beginDate")
-    ).group_by(
-        Observationfinal.recordedFrom,
-        Observationfinal.describedBy
     ).order_by(
         "beginDate"
+    ).subquery()
+
+    return db_session.query(
+        subquery
+    ).group_by(
+        "recordedFrom",
+        "describedBy"
+    ).all()
+
+
+def get_station_element_data(db_session: Session):
+    return db_session.query(
+        Stationelement.recordedFrom,
+        Stationelement.describedBy,
+        Stationelement.beginDate
+    ).order_by(Stationelement.beginDate).all()
+
+
+def sync_stationelement_with_observationfinal_sqla(db_session: Session):
+    observationfinal_grouped_rows = group_observation_final_data(
+        db_session
     )
-    insert_stmt = insert(
-        Stationelement
-    ).from_select(
-        ["recordedFrom", "describedBy", "beginDate"], select_stmt
+    station_element_rows = get_station_element_data(
+        db_session
     )
 
-    db_session.execute(str(insert_stmt).replace("INSERT INTO", "REPLACE"))
+    obsfinal_set = {
+        "#-#".join([str(row[0]), str(row[1]), str(row[2])])
+        for row in observationfinal_grouped_rows
+    }
+
+    stelement_set = {
+        "#-#".join([str(row[0]), str(row[1]), str(row[2])])
+        for row in station_element_rows
+    }
+
+    to_insert_set = obsfinal_set - stelement_set
+    to_delete_set = stelement_set - obsfinal_set
+
+    for item in to_insert_set:
+        recordedFrom, describedBy, beginDate = item.split("#-#")
+        db_session.add(Stationelement(
+            recordedFrom=recordedFrom,
+            describedBy=int(describedBy),
+            beginDate=beginDate
+        ))
     db_session.commit()
+
+    for item in to_delete_set:
+        recordedFrom, describedBy, beginDate = item.split("#-#")
+        db_session.query(
+            Stationelement
+        ).filter_by(
+            recordedFrom=recordedFrom,
+            describedBy=int(describedBy),
+            beginDate=beginDate
+        ).delete()
+        db_session.commit()
 
 
 def sync_stationelement_with_observationfinal_sql(db_session: Session):
@@ -36,12 +78,13 @@ def sync_stationelement_with_observationfinal_sql(db_session: Session):
     db_session.commit()
 
     sql = f"""
-    REPLACE {Stationelement.__tablename__}
+    INSERT INTO {Stationelement.__tablename__}
     (recordedFrom, describedBy, beginDate)
+    SELECT * FROM(
         SELECT recordedFrom, describedBy, DATE(obsDatetime) as beginDate
         FROM {Observationfinal.__tablename__}
-        GROUP BY recordedFrom, describedBy
-        ORDER BY beginDate;
+        ORDER BY beginDate ASC
+    ) t GROUP BY t.recordedFrom, t.describedBy
     """
 
     db_session.execute(sql)
@@ -67,5 +110,3 @@ if __name__ == "__main__":
     sync_stationelement_with_observationfinal_sql(session)
     sync_stationelement_with_observationfinal_sqla(session)
     session.close()
-
-
