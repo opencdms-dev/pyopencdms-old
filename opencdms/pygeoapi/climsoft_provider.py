c6b0fde40f47bc5606f1139d2dc06cf26e64c467
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-02-16 17:11:14 +0600

    use alchemyjsonschema to get fields

--- opencdms/pygeoapi/climsoft_provider.py
+++ opencdms/pygeoapi/climsoft_provider.py
@@ -12,6 +12,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Query, joinedload
 from sqlalchemy.sql import asc, desc
 from typing import Dict, List
+from alchemyjsonschema import SchemaFactory
+from alchemyjsonschema import ForeignKeyWalker
 from opencdms.utils.db import get_connection_string, get_count
 from opencdms.models.climsoft import v4_1_1_core as models
 
@@ -84,19 +86,13 @@ class DatabaseConnection:
             raise ProviderConnectionError()
 
         if self.context == 'query':
-            result = {
-                column.key: column.type.get_dbapi_type(
-                    self.engine.dialect.dbapi
-                )
-                for column in models.Observationfinal.__table__.columns
+            factory = SchemaFactory(ForeignKeyWalker)
+            self.fields = {
+                k: v['type']
+                for k, v in factory(
+                    model=models.Observationfinal
+                ).get("properties").items()
             }
-
-            if self.properties:
-                result = [res for res in result if res[0] in self.properties]
-
-            for k, v in result.items():
-                self.fields[k] = {'type': v}
-
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
