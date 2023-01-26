fb0b01be3f580c38821e15fdfa1c44aed1285c0f
Chinedu <chinedudimonyeka@gmail.com>
2023-01-26 14:57:25 +0100

    Adds table definitions

---
+++ opencdms/models/cdm.py
@@ -0,0 +1,28 @@
+class ObservationType():
+    pass
+
+class FeatureType():
+    pass
+
+class ObservedProperty():
+    pass
+
+class ObservingProcedure():
+    pass
+
+class RecordStatus():
+    pass
+class Stations():
+    pass
+
+class Sensors():
+    pass
+
+class Observations():
+    pass
+
+class Collections():
+    pass
+
+class Features():
+    pass
